b2ac9d5b168d6ee60b4fd17df3395d11e651e2b4
Tim Smith <tsmith@chef.io>
2019-02-01 15:42:36 -0800

    Pin back to rubygems 2.7.X
    Since our last pin was 2.7.7 we weren't actually running 3.x after all. We have some work to do to get ready here including upgrading chef-client.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -1,6 +1,6 @@
 override :erlang, version: "18.3.4.9"
 override :lua, version: "5.1.5"
-override :rubygems, version: "3.0.2"
+override :rubygems, version: "2.7.8"
 override :bundler, version: "1.17.3"
 override :'omnibus-ctl', version: "master"
 override :chef, version: "v14.5.33"
